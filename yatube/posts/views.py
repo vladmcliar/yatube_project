be10080c879ee3afdc85e4205b0ccca489baf16d
Влад Семёнов <vladmcliar@ya.ru>
2022-12-18 18:02:17 +0300

    Added templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def posts_list(request):
